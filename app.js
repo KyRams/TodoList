89c3223984099ece35a8eba03b80d2541085c859
Kyle Ramsay <krd.ramsay@gmail.com>
2021-06-09 16:10:22 -0600

    added filter for list items

--- app.js
+++ app.js
@@ -3,11 +3,13 @@
 const todoInput = document.querySelector('.todo-input')
 const todoButton = document.querySelector('.todo-button')
 const todoList = document.querySelector('.todo-list')
+const filterOption = document.querySelector(".filter-todo")
 
 
 //Event listeners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 //Functions
 function addTodo(event){
@@ -49,8 +51,32 @@ function deleteCheck(e){
         });
     }
     //check mark
-    
     if (item.classList[0] === "complete-btn"){
         todo.classList.toggle('completed');
     }
+}
+
+function filterTodo(e){
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo){
+        switch(e.target.value){
+            case "all":
+                todo.style.display = "flex";
+                break;
+            case "completed":
+                if (todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                }else {
+                    todo.style.display = "none";
+                }
+                break;
+            case "uncompleted":
+                if (!todo.classList.contains('completed')){
+                    todo.style.display = 'flex';
+                }else{
+                    todo.style.display = 'none';
+                }
+                break;
+        }
+    });
 }
